cd3455a779ca78a4184383a3d7e2133e777e5289
aleushea <aleushea@gmail.com>
2024-05-17 03:54:32 +0300

    0x03. Log Parsing project

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,9 +8,9 @@ import sys
 def print_data(total_file_size, status_code_data):
     """prints total size and status code count"""
     print('File size: {}'.format(total_file_size))
-    for k, v in sorted(status_code_data.items()):
-        if v != 0:
-            print('{}: {}'.format(k, v))
+    for code, count in sorted(status_code_data.items()):
+            if count != 0:
+                print('{}: {}'.format(code, count))
 
 
 status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
