373d8f3b6789f107920a4f9ea4fda41ce40af66d
aleushea <aleushea@gmail.com>
2024-05-17 04:09:09 +0300

    0x03. Log Parsing project

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,45 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-    this project is related with pythons log parsing
+This project is related to Python log parsing.
 """
 import sys
 
 
 def print_data(total_file_size, status_code_data):
-    """prints total size and status code count"""
-    print('File size: {}'.format(total_file_size))
+    """Prints the total size and status code count"""
+    print('File size:', total_file_size)
     for code, count in sorted(status_code_data.items()):
-            if count != 0:
-                print('{}: {}'.format(code, count))
+        if count != 0:
+            print(f'{code}: {count}')
 
 
-status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-status_code_data = {code: 0 for code in status_codes}
-total_file_size = 0
-try:
+def parse_logs():
+    status_codes = {'200', '301', '400', '401', '403', '404', '405', '500'}
+    status_code_data = {code: 0 for code in status_codes}
+    total_file_size = 0
     count = 0
-    for line in sys.stdin:
-        splitstr = line.split()
-        try:
-            total_file_size += int(splitstr[-1])
-            code = splitstr[-2]
-            if code in status_code_data:
-                count += 1
-                status_code_data[code] += 1
-                if count % 10 == 0:
-                    print_data(total_file_size, status_code_data)
-        except:
-            pass
-except KeyboardInterrupt:
-    print_data(total_file_size, status_code_data)
-    raise
-else:
-    print_data(total_file_size, status_code_data)
+
+    try:
+        for line in sys.stdin:
+            splitstr = line.split()
+            try:
+                total_file_size += int(splitstr[-1])
+                code = splitstr[-2]
+                if code in status_code_data:
+                    count += 1
+                    status_code_data[code] += 1
+                    if count % 10 == 0:
+                        print_data(total_file_size, status_code_data)
+            except (IndexError, ValueError):
+                pass
+
+    except KeyboardInterrupt:
+        print_data(total_file_size, status_code_data)
+        raise
+
+    else:
+        print_data(total_file_size, status_code_data)
+
+
+if __name__ == '__main__':
+    parse_logs()
