48a46eb3b69bee6f8522c9d6e2af5f169aec1ebd
aleushea <aleushea@gmail.com>
2024-05-17 04:20:21 +0300

    0x03. Log Parsing project

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,37 @@
 #!/usr/bin/env python3
-"""
-This project is related to Python log parsing.
-"""
+"""log parsing"""
 import sys
 
 
 def print_data(total_file_size, status_code_data):
-    """Prints the total size and status code count"""
-    print('File size:', total_file_size)
+    """
+	prints total size and status code count
+	"""
+    print('File size: {}'.format(total_file_size))
     for code, count in sorted(status_code_data.items()):
         if count != 0:
-            print(f'{code}: {count}')
+            print('{}: {}'.format(code, count))
 
-
-def parse_logs():
-    status_codes = {'200', '301', '400', '401', '403', '404', '405', '500'}
-    status_code_data = {code: 0 for code in status_codes}
-    total_file_size = 0
+status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+status_code_data = {code: 0 for code in status_codes}
+total_file_size = 0
+try:
     count = 0
-
-    try:
-        for line in sys.stdin:
-            splitstr = line.split()
-            try:
-                total_file_size += int(splitstr[-1])
-                code = splitstr[-2]
-                if code in status_code_data:
-                    count += 1
-                    status_code_data[code] += 1
-                    if count % 10 == 0:
-                        print_data(total_file_size, status_code_data)
-            except (IndexError, ValueError):
-                pass
-
-    except KeyboardInterrupt:
-        print_data(total_file_size, status_code_data)
-        raise
-
-    else:
-        print_data(total_file_size, status_code_data)
-
-
-if __name__ == '__main__':
-    parse_logs()
+    for line in sys.stdin:
+        splitstr = line.split()
+        try:
+            total_file_size += int(splitstr[-1])
+            code = splitstr[-2]
+            if code in status_code_data:
+                count += 1
+                status_code_data[code] += 1
+                if count % 10 == 0:
+                    print_data(total_file_size, status_code_data)
+        except:
+            pass
+except KeyboardInterrupt:
+    print_data(total_file_size, status_code_data)
+    raise
+else:
+    print_data(total_file_size, status_code_data)
+    
