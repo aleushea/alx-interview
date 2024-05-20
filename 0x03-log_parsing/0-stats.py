6a503f5b99c359a77a8dc40dfbea2721771cd12e
aleushea <aleushea@gmail.com>
2024-05-20 04:43:58 +0300

    0x03. Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics.
-
+"""this script Reads from standard input and computes metrics.
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
     - Total file size up to that point.
@@ -56,4 +55,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print_stats(size, status_codes)
         raise
-    
