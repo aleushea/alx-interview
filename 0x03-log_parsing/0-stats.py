e6a4f7ef3fc26f531024a8de5dc4cfc15ff23706
aleushea <aleushea@gmail.com>
2024-05-20 05:28:49 +0300

    0x03. Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,38 @@
 #!/usr/bin/python3
 """
-This project is related to Python log parsing.which reads from standard input and computes metrics.
-After every ten lines the script prints the following statistics:,
-    - Total file size up to that point.
-    - Count of read status codes up to that point.
+This project is related to Python log parsing.
 """
-
 import sys
+import os
 
-if __name__ == '__main__':
 
-    filesize, count = 0, 0
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    stats = {k: 0 for k in codes}
+def print_data(total_file_size, status_code_data):
+    """ This method prints total size and status code count """
+    print('File size: {}'.format(total_file_size))
+    for code, count in sorted(status_code_data.items()):
+        if count != 0:
+            print('{}: {}'.format(code, count))
 
-    def print_stats(stats: dict, file_size: int) -> None:
-        print("File size: {:d}".format(filesize))
-        for k, v in sorted(stats.items()):
-            if v:
-                print("{}: {}".format(k, v))
-
+    status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    status_code_data = {code: 0 for code in status_codes}
+    total_file_size = 0
     try:
+        count = 0
         for line in sys.stdin:
-            count += 1
-            data = line.split()
+            splitstr = line.split()
             try:
-                status_code = data[-2]
-                if status_code in stats:
-                    stats[status_code] += 1
-            except BaseException:
+                total_file_size += int(splitstr[-1])
+                code = splitstr[-2]
+                if code in status_code_data:
+                    count += 1
+                    status_code_data[code] += 1
+                    if count % 10 == 0:
+                        print_data(total_file_size, status_code_data)
+            except:
                 pass
-            try:
-                filesize += int(data[-1])
-            except BaseException:
-                pass
-            if count % 10 == 0:
-                print_stats(stats, filesize)
-        print_stats(stats, filesize)
     except KeyboardInterrupt:
-        print_stats(stats, filesize)
+        print_data(total_file_size, status_code_data)
         raise
+    else:
+        print_data(total_file_size, status_code_data)
+        
