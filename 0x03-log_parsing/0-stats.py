7e060a6a9bbc2fd87a3ce27122cb7bae9af1ba57
aleushea <aleushea@gmail.com>
2024-06-13 23:39:08 +0300

    0x03. Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -11,25 +11,25 @@ def print_data(total_file_size, status_code_data):
             print('{}: {}'.format(k, v))
 
 
-status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-status_code_data = {code: 0 for code in status_codes}
-total_file_size = 0
-try:
-    count = 0
-    for line in sys.stdin:
-        splitstr = line.split()
-        try:
-            total_file_size += int(splitstr[-1])
-            code = splitstr[-2]
-            if code in status_code_data:
-                count += 1
-                status_code_data[code] += 1
-                if count % 10 == 0:
-                    print_data(total_file_size, status_code_data)
-        except:
-            pass
-except KeyboardInterrupt:
-    print_data(total_file_size, status_code_data)
-    raise
-else:
-    print_data(total_file_size, status_code_data)
+            status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+            status_code_data = {code: 0 for code in status_codes}
+            total_file_size = 0
+            try:
+                count = 0
+                for line in sys.stdin:
+                    splitstr = line.split()
+                    try:
+                        total_file_size += int(splitstr[-1])
+                        code = splitstr[-2]
+                        if code in status_code_data:
+                            count += 1
+                            status_code_data[code] += 1
+                            if count % 10 == 0:
+                                print_data(total_file_size, status_code_data)
+                    except:
+                        pass
+            except KeyboardInterrupt:
+                print_data(total_file_size, status_code_data)
+                raise
+        else:
+            print_data(total_file_size, status_code_data)
