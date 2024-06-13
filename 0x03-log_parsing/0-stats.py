7e4b6e1332b45e2803caf8a0247ebd638eaf08dc
aleushea <aleushea@gmail.com>
2024-06-13 23:47:29 +0300

    0x03. Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,35 +1,38 @@
 #!/usr/bin/python3
-"""log parsing"""
+
+"""Log parsing"""
+
 import sys
 
-
-def print_data(total_file_size, status_code_data):
-    """prints total size and status code count"""
-    print('File size: {}'.format(total_file_size))
-    for k, v in sorted(status_code_data.items()):
-        if v != 0:
-            print('{}: {}'.format(k, v))
+STATUS_CODES = ['200', '301', '400', '401', '403', '404', '405', '500']
+STATUS_CODE_DATA = {code: 0 for code in STATUS_CODES}
+TOTAL_FILE_SIZE = 0
+COUNT = 0
 
 
-status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-status_code_data = {code: 0 for code in status_codes}
-total_file_size = 0
+def print_data():
+    """Prints total size and status code count."""
+    print(f'File size: {TOTAL_FILE_SIZE}')
+    for code, count in sorted(STATUS_CODE_DATA.items()):
+        if count != 0:
+            print(f'{code}: {count}')
+
+
 try:
-    count = 0
     for line in sys.stdin:
-        splitstr = line.split()
+        parts = line.split()
         try:
-            total_file_size += int(splitstr[-1])
-            code = splitstr[-2]
-            if code in status_code_data:
-                count += 1
-                status_code_data[code] += 1
-                if count % 10 == 0:
-                    print_data(total_file_size, status_code_data)
-        except:
+            TOTAL_FILE_SIZE += int(parts[-1])
+            code = parts[-2]
+            if code in STATUS_CODE_DATA:
+                COUNT += 1
+                STATUS_CODE_DATA[code] += 1
+                if COUNT % 10 == 0:
+                    print_data()
+        except (ValueError, IndexError):
             pass
 except KeyboardInterrupt:
-    print_data(total_file_size, status_code_data)
+    print_data()
     raise
 else:
-    print_data(total_file_size, status_code_data)
+    print_data()
