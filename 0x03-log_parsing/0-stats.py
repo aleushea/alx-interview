e34b8c5f8d029157e4671b80103ae31c19980080
aleushea <aleushea@gmail.com>
2024-06-22 04:58:57 +0300

    Edited

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,82 @@
 #!/usr/bin/python3
-"""Log Parser"""
+"""
+Script that reads stdin line by line and computes metrics
+"""
 import sys
+import re
+import signal
 
 
-if __name__ == '__main__':
-    file_size = [0]
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                    403: 0, 404: 0, 405: 0, 500: 0}
+def format_check(data):
+    """
+    Checks for the input format using regex, and
+    the line must be skipped if it does not match
+    """
+    pattern = re.compile(
+        r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - "
+        r"\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6})\] "
+        r'"GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)'
+    )
+    return bool(pattern.match(data))
 
-    def print_stats():
-        """ Print statistics """
-        print('File size: {}'.format(file_size[0]))
-        for key in sorted(status_codes.keys()):
-            if status_codes[key]:
-                print('{}: {}'.format(key, status_codes[key]))
 
-    def parse_line(line):
-        """ Checks the line for matches """
-        try:
-            line = line[:-1]
-            word = line.split(' ')
-            # File size is last parameter on stdout
-            file_size[0] += int(word[-1])
-            # Status code comes before file size
-            status_code = int(word[-2])
-            # Move through dictionary of status codes
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-        except BaseException:
-            pass
+def log_passing():
+    """
+    Reads stdin line by line and computes metrics
+    """
+    status_code = {
+        200: 0,
+        301: 0,
+        400: 0,
+        401: 0,
+        403: 0,
+        404: 0,
+        405: 0,
+        500: 0
+    }
+    total_size = 0
+    counter = 0
 
-    linenum = 1
+    def signal_handler(signum, frame):
+        """
+        Signal handler for printing the stats before exiting
+        """
+        print("File size: {}".format(total_size))
+        for key in sorted(status_code.keys()):
+            if status_code[key] != 0:
+                print("{}: {}".format(key, status_code[key]))
+        sys.exit(0)
+
+    # Register the signal handler for SIGINT (CTRL + C)
+    signal.signal(signal.SIGINT, signal_handler)
+
     try:
         for line in sys.stdin:
-            parse_line(line)
-            """ print after every 10 lines """
-            if linenum % 10 == 0:
-                print_stats()
-            linenum += 1
+            try:
+                if not format_check(line):
+                    continue
+                counter += 1
+                data = line.split()
+                total_size += int(data[-1])
+
+                status = int(data[-2])
+                if status in status_code:
+                    status_code[status] += 1
+                if counter == 10:
+                    print("File size: {}".format(total_size))
+                    for key in sorted(status_code.keys()):
+                        if status_code[key] != 0:
+                            print("{}: {}".format(key, status_code[key]))
+                    counter = 0
+
+            except Exception as e:
+                # If any other exception occurs, skip the line
+                continue
+
     except KeyboardInterrupt:
-        print_stats()
-        raise
-    print_stats()
+        # If we get a keyboard interrupt, print the stats before exiting
+        signal_handler(signal.SIGINT, None)
+
+
+if __name__ == "__main__":
+    log_passing()
